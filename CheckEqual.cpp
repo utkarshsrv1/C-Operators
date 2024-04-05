17929eba310683c4645bfc74fb7867a7c45522f7
Utkarsh Raj <109860910+utkarshsrv1@users.noreply.github.com>
2024-04-05 22:34:57 +0530

    Create CheckEqual.cpp
    Q1 - Write a program to check whether two numbers (entered by user) are equal or not.

---
+++ CheckEqual.cpp
@@ -0,0 +1,25 @@
+//Q1 - Write a program to check whether two numbers (entered by user) are equal or not.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+
+  cout<<"Enter first number"<<endl;
+  cin>>a;
+
+  cout<<"Enter second number"<<endl;
+  cin>>b;
+
+  if(a==b){
+    cout<<"1"<<endl;
+  }
+  else:
+  {
+    cout<<"0"<<endl;
+  }
+
+  return 0;
+}
